066b0a3661dbc64478b9bb62648be40b0c7f7502
burrrrakozenc <brk.ozenc@gmail.com>
2022-12-08 05:19:04 +0300

    navbar fixed

--- src/components/hero/TwoColumnWithInput.js
+++ src/components/hero/TwoColumnWithInput.js
@@ -7,13 +7,13 @@ import tw from "twin.macro";
 import Header from "../headers/light.js";
 
 import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
-import DesignIllustration from "../../images/design-illustration-2.svg";
+// import DesignIllustration from "../../images/design-illustration-2.svg";
 // import CustomersLogoStripImage from "../../images/customers-logo-strip.png";
 
 const Container = tw.div`relative`;
 const TwoColumn = tw.div`flex flex-col lg:flex-row lg:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
 const LeftColumn = tw.div`relative lg:w-5/12 text-center max-w-lg mx-auto lg:max-w-none lg:text-left`;
-const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
+// const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
 
 const Heading = tw.h1`font-bold text-3xl md:text-3xl lg:text-4xl xl:text-5xl text-gray-900 leading-tight`;
 const Paragraph = tw.p`my-5 lg:my-8 text-base xl:text-lg`;
@@ -28,7 +28,7 @@ const Paragraph = tw.p`my-5 lg:my-8 text-base xl:text-lg`;
 //   }
 // `;
 
-const IllustrationContainer = tw.div`flex justify-center lg:justify-end items-center`;
+// const IllustrationContainer = tw.div`flex justify-center lg:justify-end items-center`;
 
 // Random Decorator Blobs (shapes that you see in background)
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
