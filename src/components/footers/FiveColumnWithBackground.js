3a0e91d28aad11323447ba70483665f832910daa
burrrrakozenc <brk.ozenc@gmail.com>
2022-12-08 05:15:04 +0300

    warnings fixed3

--- src/components/footers/FiveColumnWithBackground.js
+++ src/components/footers/FiveColumnWithBackground.js
@@ -1,6 +1,6 @@
 import React from "react";
 import tw from "twin.macro";
-import styled from "styled-components";
+// import styled from "styled-components";
 // import { css } from "styled-components/macro"; //eslint-disable-line
 
 import LogoImage from "images/comeSocialLogo.png";
