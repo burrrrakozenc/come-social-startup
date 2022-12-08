38ec6b798301321625ec171cab9ee0b844f16152
burrrrakozenc <brk.ozenc@gmail.com>
2022-12-08 05:39:33 +0300

    navbar fixed

--- src/components/hero/TwoColumnWithInput.js
+++ src/components/hero/TwoColumnWithInput.js
@@ -1,12 +1,12 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import tw from "twin.macro";
 //eslint-disable-next-line
 // import { css } from "styled-components/macro";
 
 import Header from "../headers/light.js";
 
-import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
+// import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
 // import DesignIllustration from "../../images/design-illustration-2.svg";
 // import CustomersLogoStripImage from "../../images/customers-logo-strip.png";
 
