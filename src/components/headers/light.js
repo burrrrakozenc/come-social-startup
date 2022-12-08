d13d076093e59ac1d7f0c9e7a3c666e10560ee50
burrrrakozenc <brk.ozenc@gmail.com>
2022-12-08 05:38:01 +0300

    navbar fixed

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import tw from "twin.macro";
 import styled from "styled-components";
 // import { css } from "styled-components/macro"; //eslint-disable-line
@@ -8,6 +8,7 @@ import styled from "styled-components";
 
 // import logo from "../../images/logo.svg";
 import logo from "../../images/comeSocialLogo.png";
+import {useWindowSize} from "../../static/functions";
 // import { ReactComponent as MenuIcon } from "feather-icons/dist/icons/menu.svg";
 // import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
 
@@ -57,64 +58,68 @@ export const DesktopNavLinks = tw.nav`
   hidden lg:flex flex-1 justify-between items-center
 `;
 
-export default ({ roundedHeaderButton = false, logoLink, links, className, collapseBreakpointClass = "lg" }) => {
-  /*
-   * This header component accepts an optionals "links" prop that specifies the links to render in the navbar.
-   * This links props should be an array of "NavLinks" components which is exported from this file.
-   * Each "NavLinks" component can contain any amount of "NavLink" component, also exported from this file.
-   * This allows this Header to be multi column.
-   * So If you pass only a single item in the array with only one NavLinks component as root, you will get 2 column header.
-   * Left part will be LogoLink, and the right part will be the the NavLinks component you
-   * supplied.
-   * Similarly if you pass 2 items in the links array, then you will get 3 columns, the left will be "LogoLink", the center will be the first "NavLinks" component in the array and the right will be the second "NavLinks" component in the links array.
-   * You can also choose to directly modify the links here by not passing any links from the parent component and
-   * changing the defaultLinks variable below below.
-   * If you manipulate links here, all the styling on the links is already done for you. If you pass links yourself though, you are responsible for styling the links or use the helper styled components that are defined here (NavLink)
-   */
-  // const defaultLinks = [
-  //   <NavLinks key={1}>
-  //     <NavLink href="/#">About</NavLink>
-  //     <NavLink href="/#">Blog</NavLink>
-  //     <NavLink href="/#">Pricing</NavLink>
-  //     <NavLink href="/#">Contact Us</NavLink>
-  //     <NavLink href="/#" tw="lg:ml-12!">
-  //       Login
-  //     </NavLink>
-  //     <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#">Sign Up</PrimaryLink>
-  //   </NavLinks>
-  // ];
+export default ({ logoLink, className, collapseBreakpointClass = "lg"}) => {
+    /*
+     * This header component accepts an optionals "links" prop that specifies the links to render in the navbar.
+     * This links props should be an array of "NavLinks" components which is exported from this file.
+     * Each "NavLinks" component can contain any amount of "NavLink" component, also exported from this file.
+     * This allows this Header to be multi column.
+     * So If you pass only a single item in the array with only one NavLinks component as root, you will get 2 column header.
+     * Left part will be LogoLink, and the right part will be the the NavLinks component you
+     * supplied.
+     * Similarly if you pass 2 items in the links array, then you will get 3 columns, the left will be "LogoLink", the center will be the first "NavLinks" component in the array and the right will be the second "NavLinks" component in the links array.
+     * You can also choose to directly modify the links here by not passing any links from the parent component and
+     * changing the defaultLinks variable below below.
+     * If you manipulate links here, all the styling on the links is already done for you. If you pass links yourself though, you are responsible for styling the links or use the helper styled components that are defined here (NavLink)
+     */
+    // const defaultLinks = [
+    //   <NavLinks key={1}>
+    //     <NavLink href="/#">About</NavLink>
+    //     <NavLink href="/#">Blog</NavLink>
+    //     <NavLink href="/#">Pricing</NavLink>
+    //     <NavLink href="/#">Contact Us</NavLink>
+    //     <NavLink href="/#" tw="lg:ml-12!">
+    //       Login
+    //     </NavLink>
+    //     <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#">Sign Up</PrimaryLink>
+    //   </NavLinks>
+    // ];
 
-  // const { showNavLinks, animation, toggleNavbar } = useAnimatedNavToggler();
-  const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
+    // const { showNavLinks,  toggleNavbar } = useAnimatedNavToggler();
+    const collapseBreakpointCss = collapseBreakPointCssMap[collapseBreakpointClass];
 
-  const defaultLogoLink = (
-    <LogoLink href="/">
-      <img style={{width:170}} src={logo} alt="logo" />
-      COME SOCIAL
-    </LogoLink>
-  );
+    const sizes = useWindowSize()
 
-  logoLink = logoLink || defaultLogoLink;
-  // links = links || defaultLinks;
+    const defaultLogoLink = (
+        <LogoLink href="/">
+            <img style={{width: 170}} src={logo} alt="logo"/>
+            COME SOCIAL
+        </LogoLink>
+    );
 
-  return (
-    <Header className={className || "header-light"}>
-      <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
-        {logoLink}
-        {/*{links}*/}
-      </DesktopNavLinks>
+    logoLink = logoLink || defaultLogoLink;
+    // links = links || defaultLinks;
 
-      <MobileNavLinksContainer css={collapseBreakpointCss.mobileNavLinksContainer}>
-        {logoLink}
-        {/*<MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>*/}
-        {/*  {links}*/}
-        {/*</MobileNavLinks>*/}
-        {/*<NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>*/}
-        {/*  {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}*/}
-        {/*</NavToggle>*/}
-      </MobileNavLinksContainer>
-    </Header>
-  );
+    return (
+        <Header className={className || "header-light"}>
+            {/*{sizes.width < 680 ?*/}
+                <DesktopNavLinks css={collapseBreakpointCss.desktopNavLinks}>
+                    {logoLink}
+                    {/*{links}*/}
+                </DesktopNavLinks>
+              :
+              <MobileNavLinksContainer style={sizes.width > 680 ? {display:'none'} : {}} css={collapseBreakpointCss.mobileNavLinksContainer}>
+                  {logoLink}
+                  {/*<MobileNavLinks initial={{ x: "150%", display: "none" }} animate={animation} css={collapseBreakpointCss.mobileNavLinks}>*/}
+                  {/*  {links}*/}
+                  {/*</MobileNavLinks>*/}
+                  {/*<NavToggle onClick={toggleNavbar} className={showNavLinks ? "open" : "closed"}>*/}
+                  {/*  {showNavLinks ? <CloseIcon tw="w-6 h-6" /> : <MenuIcon tw="w-6 h-6" />}*/}
+                  {/*</NavToggle>*/}
+              </MobileNavLinksContainer>
+
+        </Header>
+    );
 };
 
 /* The below code is for generating dynamic break points for navbar.
@@ -124,24 +129,24 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
  */
 
 const collapseBreakPointCssMap = {
-  sm: {
-    mobileNavLinks: tw`sm:hidden`,
-    desktopNavLinks: tw`sm:flex`,
-    mobileNavLinksContainer: tw`sm:hidden`
-  },
-  md: {
-    mobileNavLinks: tw`md:hidden`,
-    desktopNavLinks: tw`md:flex`,
-    mobileNavLinksContainer: tw`md:hidden`
-  },
-  lg: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  },
-  xl: {
-    mobileNavLinks: tw`lg:hidden`,
-    desktopNavLinks: tw`lg:flex`,
-    mobileNavLinksContainer: tw`lg:hidden`
-  }
+    sm: {
+        mobileNavLinks: tw`sm:hidden`,
+        desktopNavLinks: tw`sm:flex`,
+        mobileNavLinksContainer: tw`sm:hidden`
+    },
+    md: {
+        mobileNavLinks: tw`md:hidden`,
+        desktopNavLinks: tw`md:flex`,
+        mobileNavLinksContainer: tw`md:hidden`
+    },
+    lg: {
+        mobileNavLinks: tw`lg:hidden`,
+        desktopNavLinks: tw`lg:flex`,
+        mobileNavLinksContainer: tw`lg:hidden`
+    },
+    xl: {
+        mobileNavLinks: tw`lg:hidden`,
+        desktopNavLinks: tw`lg:flex`,
+        mobileNavLinksContainer: tw`lg:hidden`
+    }
 };
--- src/components/hero/TwoColumnWithInput.js
+++ src/components/hero/TwoColumnWithInput.js
@@ -13,7 +13,7 @@ import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-
 const Container = tw.div`relative`;
 const TwoColumn = tw.div`flex flex-col lg:flex-row lg:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
 const LeftColumn = tw.div`relative lg:w-5/12 text-center max-w-lg mx-auto lg:max-w-none lg:text-left`;
-// const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
+const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
 
 const Heading = tw.h1`font-bold text-3xl md:text-3xl lg:text-4xl xl:text-5xl text-gray-900 leading-tight`;
 const Paragraph = tw.p`my-5 lg:my-8 text-base xl:text-lg`;
@@ -31,9 +31,9 @@ const Paragraph = tw.p`my-5 lg:my-8 text-base xl:text-lg`;
 // const IllustrationContainer = tw.div`flex justify-center lg:justify-end items-center`;
 
 // Random Decorator Blobs (shapes that you see in background)
-const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
-  ${tw`pointer-events-none opacity-5 absolute left-0 bottom-0 h-64 w-64 transform -translate-x-2/3 -z-10`}
-`;
+// const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
+//   ${tw`pointer-events-none opacity-5 absolute left-0 bottom-0 h-64 w-64 transform -translate-x-2/3 -z-10`}
+// `;
 
 // const CustomersLogoStrip = styled.div`
 //   ${tw`mt-12 lg:mt-20`}
@@ -68,13 +68,13 @@ export default ({ roundedHeaderButton }) => {
             {/*  <img src={CustomersLogoStripImage} alt="Our Customers" />*/}
             {/*</CustomersLogoStrip>*/}
           </LeftColumn>
-          {/*<RightColumn>*/}
+          <RightColumn>
           {/*  <IllustrationContainer>*/}
           {/*    <img tw="min-w-0 w-full max-w-lg xl:max-w-3xl" src={DesignIllustration} alt="Design Illustration" />*/}
           {/*  </IllustrationContainer>*/}
-          {/*</RightColumn>*/}
+          </RightColumn>
         </TwoColumn>
-        <DecoratorBlob1 />
+        {/*<DecoratorBlob1 />*/}
       </Container>
     </>
   );
